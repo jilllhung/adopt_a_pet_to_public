284f68fad8982b16c380d781af3e02d942c341c9
hannahtonnessen <76666250+hannahtonnessen@users.noreply.github.com>
2021-04-27 18:55:45 -0600

    test model added

--- adopt_a_pet/src/main/java/com/team/adopt_a_pet/controllers/PetController.java
+++ adopt_a_pet/src/main/java/com/team/adopt_a_pet/controllers/PetController.java
@@ -1,34 +1,58 @@
 package com.team.adopt_a_pet.controllers;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.team.adopt_a_pet.models.Test;
 @SpringBootApplication
 
-@RestController
+@RestController //parses object and turns into json for you and then sends it off.
 public class PetController {
 	public static void main(String[] args) {
 		SpringApplication.run(PetController.class, args);
 	}
 	
+	@RequestMapping("/getPets") 
 	
-    @RequestMapping("/getPets")
-    public String getPets() {
-    	try {
-			URL url = new URL("https://api.rescuegroups.org/v5/public/animals/search/available/cats/haspic?fields[cats]=distance&include=breeds,locations&sort=random&limit=1");
-//			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            conn.setRequestMethod("GET");
-//            conn.connect();
-			System.out.println(url);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "pets";
-    
-    	}
+//	public Test getPets() {
+//		Test test = new Test();
+//		test.setName("hello");
+//				return test;
+		//grab object data and convert to json
+		// return json object as a string
+	//}
+	public List<Test> test() {
+		List<Test> test = new ArrayList<>(); //arraylist inherits from list
+		Test dog = new Test();
+		dog.setName("daisy");
+		Test cat = new Test();
+		cat.setName("Clover");
+		test.add(dog);
+		test.add(cat);
+				return test;
+		//grab object data and convert to json
+		// return json object as a string
+	}
+	
+//    @RequestMapping("/getPets")
+//    public String getPets() {
+//    	try {
+//			URL url = new URL("https://api.rescuegroups.org/v5/public/animals/search/available/cats/haspic?fields[cats]=distance&include=breeds,locations&sort=random&limit=1");
+////			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+////            conn.setRequestMethod("GET");
+////            conn.connect();
+//			System.out.println(url);
+//		} catch (MalformedURLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return "pets";
+//    
+//    	}
     }
---
+++ adopt_a_pet/src/main/java/com/team/adopt_a_pet/models/Test.java
@@ -0,0 +1,43 @@
+package com.team.adopt_a_pet.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tests")
+public class Test {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	@NotNull
+	private String name;
+	private String description;
+	public Test(){
+		
+	}
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
