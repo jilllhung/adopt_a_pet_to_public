bc4bfb5595cf9ab475e595a7ef3e79e5bcb3b651
Hai Qi <53511724+0xhqli@users.noreply.github.com>
2021-04-28 11:11:26 -0700

    Update PetList.jsx

--- client/src/components/PetList.jsx
+++ client/src/components/PetList.jsx
@@ -13,31 +13,73 @@ const useStyles = makeStyles({
         maxWidth:"100vw",
         flexWrap:"wrap",
     },
-  });
+    form: {
+        margin: "auto",
+        display:"flex",
+        justifyContent:"space-between",
+        gap:"15px",
+        padding:"15px",
+    },
+    formEle: {
+        margin: "auto",
+        display:"flex",
+        justifyContent:"space-between",
+        gap:"15px",
+        padding:"15px",
+    },
+});
 
 export default (props)=>{
     const classes = useStyles();
     let [pets,setPets]=useState([]);
-    let x=async ()=>{
-        try{
-            let z=await axios.get("http://localhost:8080/pets/all");
-            console.log(z);
-            setPets(z.data);
-        }
-        catch(e){
-            console.log(e)
-        }
+    let [age,setAge]=useState("")
+    let urlDict={
+        "":"http://localhost:8080/pets/all",
+        "young":"http://localhost:8080/pets/agegroup/young",
+        "adult":"http://localhost:8080/pets/agegroup/adult",
+        "senior":"http://localhost:8080/pets/agegroup/senior",
+
     }
     useEffect(()=>{
+        let loaded=true;
+        let x=async ()=>{
+            if(urlDict[age]){
+                try{
+                    let z=await axios.get(urlDict[age]);
+                    console.log(z);
+                    if(loaded)setPets(z.data);
+                }
+                catch(e){
+                    console.log(e)
+                }
+            }
+        }
         x();
-    },[])
+        return ()=>{loaded=false;}
+    },[age])
+    let AgeSelect=(e)=>{
+        console.log(e.target.value);
+        setAge(e.target.value);
+    }
     return(
-        <div className={classes.showdiv}>
+        <div >
+            <div className={classes.form}>
+                <div className={classes.formEle}>
+                    <label htmlFor="ageGroup">Age Group</label>
+                    <select onChange={AgeSelect} value={age}>
+                        <option value="">--------</option>
+                        <option value="young">Young</option>
+                        <option value="adult">Adult</option>
+                        <option value="senior">Senior</option>
+                    </select>
+                </div>
+            </div>
+            <div className={classes.showdiv}>
             {
             pets.map((pet,i)=>
                 <Pet key={i} pet={pet}/>
-            )
-            }
+            )}
+            </div>
         </div>
     )
 }
