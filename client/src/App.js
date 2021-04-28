8932dccebda87be7312f7e51679de9198782829b
hannahtonnessen <76666250+hannahtonnessen@users.noreply.github.com>
2021-04-28 16:53:10 -0600

    made changes to App.js created Home component and created pet form component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import PetList from './components/PetList';
 import {Router} from '@reach/router';
 import ShowPet from './components/ShowPet';
 import PetAdoptionForm from './components/PetAdoptionForm';
+import Home from './components/Home';
 
 function App() {
   
   return (
     <div className="App">
       <Router>
+        <Home path="/"/>
         <ShowPet path="/animals/:id"/>
         <PetList path="/animals" default/>
         <PetAdoptionForm path="/animal_form"/>
