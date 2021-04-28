147adb4541ae1f724c64466a15da2df0a705fa49
Hai Qi <53511724+0xhqli@users.noreply.github.com>
2021-04-27 22:58:58 -0700

    frontend updates

--- client/src/components/Pet.jsx
+++ client/src/components/Pet.jsx
@@ -1,8 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+        display: 'inline-block',
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
 
 export default (props)=>{
+    const classes = useStyles();
     return(
-        <div className="pet">
-            <p>{props.pet.name}</p>
-        </div>
+        // <div className="pet">
+        //     <p>{props.pet.name}</p>
+        // </div>
+        <Card className={classes.root} variant="outlined">
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Looking for a good home
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    {props.pet.name}
+                </Typography>
+                {/* <Typography className={classes.pos} color="textSecondary">
+                adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+                </Typography> */}
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     )
 }
