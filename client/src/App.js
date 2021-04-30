dd6d629f4eb66a8be35f32c5aea34ae8e8a59b29
hannahtonnessen <76666250+hannahtonnessen@users.noreply.github.com>
2021-04-29 20:47:25 -0600

    changed app.js, added to home component

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,63 @@ import {Router} from '@reach/router';
 import ShowPet from './components/ShowPet';
 import PetAdoptionForm from './components/PetAdoptionForm';
 import Home from './components/Home';
+import Test from './components/Test';
 import { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import PetsIcon from '@material-ui/icons/Pets';
 
 function App() {
   const [spec, setSpec] = useState("all");
+
+  const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #1F368B 30%, #6363F7 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  });
+
+  
+  const classes = useStyles();
   return (
-    <div className="App" style={{width : "970px", margin : "0px auto", outline : "dotted red 2px"}}>
-      <header style={{width : "970px", height : "50px", color : "red"}}>
-        <h1>Website Name</h1>
+    <div className="App" style={{width : "970px", margin : "0px auto", backgroundColor : "#EDEDED"}}>
+      <header style={{width : "970px", height : "125px", backgroundColor : "#22327B", color : "white"}}>
+        <h1 style={{paddingTop : "35px", paddingLeft : "30px"}}>Website Name</h1>
       </header>
-      <div style={{display: "flex", marginLeft : "200px", marginTop : "75px"}}>
-        <button  type="button" class="btn btn-primary" style={{margin : "10px"}} onClick={(e) => setSpec("dog")}>Find a Dog</button>
-        <button  class="btn btn-primary" style={{margin : "10px"}} onClick={(e) => setSpec("cat")}>Find a Cat</button>
-        <button type="button" class="btn btn-primary" style={{margin : "10px"}}>Do you know of a pet that needs a new home?</button>
+      <div style={{display: "flex", marginLeft : "160px", marginTop : "25px"}}>
+        <Button classes={{
+        root: classes.root, // class name, e.g. `classes-nesting-root-x`
+        label: classes.label // class name, e.g. `classes-nesting-label-x`
+        }} type="button" style={{margin : "20px", height : "100px", width : "150px"}} onClick={(e) => setSpec("dog")}>Find a Dog
+        </Button>
+
+        <Button classes={{
+        root: classes.root, // class name, e.g. `classes-nesting-root-x`
+        label: classes.label // class name, e.g. `classes-nesting-label-x`
+        }}  style={{margin : "20px", height : "100px", width : "150px"}} onClick={(e) => setSpec("cat")}>Find a Cat
+        </Button>
+
+        <Button classes={{
+        root: classes.root, // class name, e.g. `classes-nesting-root-x`
+        label: classes.label // class name, e.g. `classes-nesting-label-x`
+        }} type="button"  style={{margin : "20px", height : "100px" ,width : "200px"}}>Do you know of a pet that needs a new home?
+        </Button>
+
       </div>
       <Router>
-        <Home path="/"/>
+        <Home path="/"/> 
         <ShowPet path="/animals/:id"/>
         <PetList path="/animals" default spec = {spec}/>
         <PetAdoptionForm path="/animal_form"/>
+        {/* <Test path="/test"/>  */}
       </Router>
     </div>
   );
---
+++ client/src/components/Test.jsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import Input from '@material-ui/core/Input';
+// import { makeStyles } from '@material-ui/core/styles';
+// export default() => {
+
+
+//     const useStyles = makeStyles((theme: Theme) => createStyles({
+//         root: {
+//           backgroundColor: theme.color.red,
+//         },
+//       }));
+//     const classes = useStyles();
+//   return (
+//     <div>
+//         <form className={classes.root} noValidate autoComplete="off">
+//             <Input defaultValue="Hello world" inputProps={{ 'aria-label': 'description' }} />
+//             <Input placeholder="Placeholder" inputProps={{ 'aria-label': 'description' }} />
+//             <Input defaultValue="Disabled" disabled inputProps={{ 'aria-label': 'description' }} />
+//             <Input defaultValue="Error" error inputProps={{ 'aria-label': 'description' }} />
+//         </form>
+
+//     </div>
+//   );
+// }
