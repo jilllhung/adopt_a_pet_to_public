f008a4032ab4915473571c88470d3d7bc8925bdb
Caleb A Comstock <c_comstock@rocketmail.com>
2021-04-30 05:20:46 -0700

    UI updates, mostly in PetAdoptionForm

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,67 +1,72 @@
-import React, {useState} from 'react';
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default() => {
-    var items = [
-        {
-            name: "https://i.dailymail.co.uk/i/pix/2013/06/27/article-2349770-1A89D9DC000005DC-814_634x420.jpg",
+const Home = () => {
+	var items = [
+		{
+			name:
+				"https://i.dailymail.co.uk/i/pix/2013/06/27/article-2349770-1A89D9DC000005DC-814_634x420.jpg",
+		},
+		{
+			name:
+				"https://media.istockphoto.com/photos/family-having-fun-outdoor-with-dog-and-basketball-ball-picture-id1154596591?k=6&m=1154596591&s=612x612&w=0&h=UPwvalJPUoIdGMpahgkkzN_ORYd4B8gStk9_PoN8fjM=",
+		},
+		{
+			name:
+				"https://resc-files-prod.s3.us-west-1.amazonaws.com/s3fs-public/styles/large/public/2018-12/gray-white-cat-worried_1.jpg?itok=gyRH7EIW",
+		},
+		{
+			name:
+				"https://www.nydailynews.com/resizer/YAT7iKQZMo7oPX0r1vcEWtUtTqo=/800x534/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/GQAMIZMB4AEAAMEYTRPVNM2ZVE.jpg",
+		},
+		{
+			name:
+				"https://g77v3827gg2notadhhw9pew7-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/are-beagles-good-family-dogs_canna-pet-e1510609386664.jpg",
+		},
+		{
+			name:
+				"https://www.worldsbestcatlitter.com/WBCL/wp-content/uploads/2019/07/body-img_4-match-1.jpg",
+		},
+		{
+			name:
+				"https://resveralife.com/wp-content/uploads/2015/08/Vine-Vera-Best-Pets-For-Your-Family-Dogs.jpg",
+		},
+	];
 
-        },
-        {
-            name: "https://media.istockphoto.com/photos/family-having-fun-outdoor-with-dog-and-basketball-ball-picture-id1154596591?k=6&m=1154596591&s=612x612&w=0&h=UPwvalJPUoIdGMpahgkkzN_ORYd4B8gStk9_PoN8fjM=",
-        },
-        {
-            name: "https://resc-files-prod.s3.us-west-1.amazonaws.com/s3fs-public/styles/large/public/2018-12/gray-white-cat-worried_1.jpg?itok=gyRH7EIW"
-        },
-        {
-            name: "https://www.nydailynews.com/resizer/YAT7iKQZMo7oPX0r1vcEWtUtTqo=/800x534/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/GQAMIZMB4AEAAMEYTRPVNM2ZVE.jpg"
-        },
-        {
-            name : "https://g77v3827gg2notadhhw9pew7-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/are-beagles-good-family-dogs_canna-pet-e1510609386664.jpg"
-        },
-        {
-            name : "https://www.worldsbestcatlitter.com/WBCL/wp-content/uploads/2019/07/body-img_4-match-1.jpg"
-        },
-        {
-            name : "https://resveralife.com/wp-content/uploads/2015/08/Vine-Vera-Best-Pets-For-Your-Family-Dogs.jpg"
-        }
-    ]
+	const useStyles = makeStyles({
+		start: {
+			display: "flex",
+			justifyContent: "center",
+		},
+		paper: {
+			backgroundColor: "#EDEDED",
+			margin: "0px",
+			padding: "0px",
+		},
+		img: {
+			width: "75vw",
+			minWidth: "690px",
+		},
+	});
 
-    const useStyles = makeStyles({
-        start:{
-            display:"flex",
-            justifyContent:"center",
-        },
-        paper:{
-            backgroundColor : "#EDEDED",
-            margin:"0px",
-            padding:"0px",
-        },
-        img:{
-            width : "75vw",
-            minWidth:"690px",
-        }
-    });
-    
-    const classes = useStyles();
+	const classes = useStyles();
 
-    return(
-        <div className={classes.start}>
-            <Carousel >
-            {
-                items.map((item, i) => 
-                    <Paper className={classes.paper}>
-                        <img className={classes.img} src={item.name}/>
-                        {/* <p>{item.description}</p> */}
-                        {/* <Button className="CheckButton">
+	return (
+		<div className={classes.start}>
+			<Carousel>
+				{items.map((item, i) => (
+					<Paper className={classes.paper}>
+						<img className={classes.img} src={item.name} alt='' />
+						{/* <p>{item.description}</p> */}
+						{/* <Button className="CheckButton">
                         </Button> */}
-                    </Paper>
-                )
-            }
-            </Carousel>
-        </div>
-    )
-}
+					</Paper>
+				))}
+			</Carousel>
+		</div>
+	);
+};
 
+export default Home;
--- client/src/components/PetList.jsx
+++ client/src/components/PetList.jsx
@@ -1,100 +1,119 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles'; //styling for react similar to bootstrap
-import Pet from './Pet';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles"; //styling for react similar to bootstrap
+import Pet from "./Pet";
 
 const useStyles = makeStyles({
-    showdiv: {
-        margin: "auto",
-        display:"flex",
-        gap:"15px",
-        padding:"15px",
-        maxWidth:"100vw",
-        flexWrap:"wrap",
-    },
-    form: {
-        margin: "auto",
-        display:"flex",
-        justifyContent:"center",
-        gap:"15px",
-        padding:"15px",
-        // border:"red dotted 1px"
-    },
-    formEle: {
-        display:"flex",
-        gap:"15px",
-        padding:"15px",
-        // border:"red dotted 1px"
-    },
-
+	showdiv: {
+		margin: "auto",
+		display: "flex",
+		gap: "15px",
+		padding: "15px",
+		maxWidth: "100vw",
+		flexWrap: "wrap",
+	},
+	form: {
+		margin: "auto",
+		display: "flex",
+		justifyContent: "center",
+		gap: "15px",
+		padding: "15px",
+		// border:"red dotted 1px"
+	},
+	formEle: {
+		display: "flex",
+		gap: "15px",
+		padding: "15px",
+		// border:"red dotted 1px"
+	},
 });
 
-export default (props)=>{
-    const classes = useStyles();
-    let [pets,setPets]=useState([]);
-    let [breedList,setBreedList]=useState([]);
-    let [breed,setBreed]=useState("");
-    let [age,setAge]=useState("");
-    console.log(props)
-    useEffect(()=>{
-        let loaded=true;
-        let x=async ()=>{
-            try{
-                let pls=await axios.get(`http://localhost:8080/pets/${props.spec}`);
-                let bls=await axios.get(`http://localhost:8080/breeds/species/${props.spec}`);
-                console.log(pls);
-                console.log(bls);
-                if(loaded){
-                    setPets(pls.data);
-                    setBreedList(bls.data)
-                    setBreed("");
-                }
-            }
-            catch(e){
-                console.log(e)
-            }
-        }
-        x();
-        return ()=>{loaded=false;}
-    },[props.spec])
-    let AgeSelect=(e)=>{
-        console.log(e.target.value);
-        setAge(e.target.value);
-    }
-    return(
-        <div>
-            <div className={classes.form}>
-                <div className={classes.formEle}>
-                    <label htmlFor="ageGroup">Age Group</label>
-                    <select onChange={AgeSelect} value={age}>
-                        <option value="">--------</option>
-                        <option value="young">Young</option>
-                        <option value="adult">Adult</option>
-                        <option value="senior">Senior</option>
-                    </select>
-                </div>
-                <div className={classes.formEle}>
-                    <label htmlFor="breed">Breed</label>
-                    <select onChange={(e)=>setBreed(e.target.value)} value={breed}>
-                        <option value="">--------</option>
-                        {breedList.map((br,i)=>(
-                            <option key={i} value={br.name}>{br.name}</option>
-                        )
-                        )}
-                    </select>
-                </div>
-            </div>
-            <h1 style={{margin:"15px"}}>Adopt Me Please</h1>
-            <div className={classes.showdiv}>
-            {
-            pets
-            .filter(pet=>age===""||pet.ageGroup.name.toLowerCase()===age)
-            .filter(pet=> breed===""||(pet.breedPrimary&&pet.breedPrimary.name===breed)||(pet.breedSecondary&&pet.breedSecondary.name===breed))
-            .map((pet,i)=>
-                <Pet key={i} pet={pet}/>
-            )}
-            </div>
-        </div>
-    )
-}
+const PetList = (props) => {
+	const classes = useStyles();
+	let [pets, setPets] = useState([]);
+	let [breedList, setBreedList] = useState([]);
+	let [breed, setBreed] = useState("");
+	let [age, setAge] = useState("");
+	console.log(props);
+	useEffect(() => {
+		let loaded = true;
+		let x = async () => {
+			try {
+				let pls = await axios.get(
+					`http://localhost:8080/pets/${props.spec}`
+				);
+				let bls = await axios.get(
+					`http://localhost:8080/breeds/species/${props.spec}`
+				);
+				console.log(pls);
+				console.log(bls);
+				if (loaded) {
+					setPets(pls.data);
+					setBreedList(bls.data);
+					setBreed("");
+				}
+			} catch (e) {
+				console.log(e);
+			}
+		};
+		x();
+		return () => {
+			loaded = false;
+		};
+	}, [props.spec]);
+	let AgeSelect = (e) => {
+		console.log(e.target.value);
+		setAge(e.target.value);
+	};
+	return (
+		<div>
+			<div className={classes.form}>
+				<div className={classes.formEle}>
+					<label htmlFor='ageGroup'>Age Group</label>
+					<select onChange={AgeSelect} value={age}>
+						<option value=''>--------</option>
+						<option value='young'>Young</option>
+						<option value='adult'>Adult</option>
+						<option value='senior'>Senior</option>
+					</select>
+				</div>
+				<div className={classes.formEle}>
+					<label htmlFor='breed'>Breed</label>
+					<select
+						onChange={(e) => setBreed(e.target.value)}
+						value={breed}
+					>
+						<option value=''>--------</option>
+						{breedList.map((br, i) => (
+							<option key={i} value={br.name}>
+								{br.name}
+							</option>
+						))}
+					</select>
+				</div>
+			</div>
+			<h1 style={{ margin: "15px" }}>Adopt Me Please</h1>
+			<div className={classes.showdiv}>
+				{pets
+					.filter(
+						(pet) =>
+							age === "" ||
+							pet.ageGroup.name.toLowerCase() === age
+					)
+					.filter(
+						(pet) =>
+							breed === "" ||
+							(pet.breedPrimary &&
+								pet.breedPrimary.name === breed) ||
+							(pet.breedSecondary &&
+								pet.breedSecondary.name === breed)
+					)
+					.map((pet, i) => (
+						<Pet key={i} pet={pet} />
+					))}
+			</div>
+		</div>
+	);
+};
+
+export default PetList;
